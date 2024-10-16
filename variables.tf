cb27cc0f141494979b2e52a30134fa67cce390fd
ptty <ptty@pttyui-MacBookPro.local>
2024-10-16 10:48:30 +0900

    fix varibales

--- variables.tf
+++ variables.tf
@@ -1,94 +1,92 @@
 variable "resource_group_location" {
   type        = string
-  default     = "koreacentral"  # 지역 설정 (한국 중부)
+  default     = "koreacentral"
 }
 
 variable "prefix" {
-  type        = string
-  default     = "tiu-axcoe-hr"  # 모든 리소스에 사용될 prefix
+  type = string
+  default = "tiu-axcoe-hr"
 }
 
-# 네트워크 관련 리소스 그룹 및 네이밍
 variable "network_resource_group_name" {
   type        = string
-  default     = "${var.prefix}-net-rg"  # 네트워크 리소스 그룹 이름
+  default     = "tiu-axcoe-hr-net-rg"
 }
 
-# AKS 관련 리소스 그룹 및 네이밍
 variable "aks_resource_group_name" {
   type        = string
-  default     = "${var.prefix}-aks-rg"  # AKS 리소스 그룹 이름
+  default     = "tiu-axcoe-hr-aks-rg"
 }
 
 variable "aks_node_resource_group_name" {
   type        = string
-  default     = "${var.prefix}-aks-nodes-rg"  # AKS 노드 리소스 그룹 이름
+  default     = "tiu-axcoe-hr-aks-nodes-rg"
 }
 
 variable "aks_cluster_name" {
   type        = string
-  default     = "${var.prefix}-aks"  # AKS 클러스터 이름
+  default     = "tiu-axcoe-hr-aks"
 }
 
-# 공통 자원 관련 리소스 그룹 및 ACR 네이밍
 variable "common_resource_group_name" {
   type        = string
-  default     = "${var.prefix}-com-rg"  # 공통 리소스 그룹 이름
+  default     = "tiu-axcoe-hr-com-rg"
 }
 
 variable "common_base_acr_name" {
   type        = string
-  default     = "${var.prefix}basecr"  # 기본 ACR 이름
+  default     = "tiuaxcoebasecr"
 }
 
 variable "common_app_acr_name" {
   type        = string
-  default     = "${var.prefix}appcr"  # 앱 ACR 이름
+  default     = "tiuaxcoeappcr"
 }
 
-# 네트워크 및 서브넷 관련 변수
 variable "aks_vnet_name" {
-  type        = string
-  default     = "${var.prefix}-vnet"
+  type = string
+  default = "tiu-axcoe-hr-vnet"
 }
 
 variable "aks_subnet_name" {
-  type        = string
-  default     = "${var.prefix}-aks-snet"
+  type = string
+  default = "tiu-axcoe-hr-aks-snet"
 }
 
 variable "aks_subnet_nsg_name" {
-  type        = string
-  default     = "${var.prefix}-nsg"
+  type = string
+  default = "tiu-axcoe-hr-nsg"
 }
 
-# AKS 클러스터 네트워크 설정
 variable "aks_pod_cidr" {
-  type        = string
-  default     = "172.22.0.0/16"
+  type = string
+  default = "172.22.0.0/16"
 }
 
 variable "aks_service_cidr" {
-  type        = string
-  default     = "192.168.132.0/24"  # 서비스 CIDR 설정
+  type = string
+  default = "192.168.132.0/24"
 }
 
 variable "aks_dns_service_ip" {
-  type        = string
-  default     = "192.168.132.10"  # DNS 서비스 IP
+  type = string
+  default = "192.168.132.10"
 }
 
-# 노드 풀 관련 설정
 variable "default_node_count" {
   type        = number
-  default     = 1  # 기본 노드 수
+  default     = 1
 }
 
-# variable "devops_node_count" {
-#  type        = number
-#  default     = 1  # DevOps 노드 수
-# }
+variable "devops_node_count" {
+  type        = number
+  default     = 1
+}
 
+variable "sre_node_count" {
+  type        = number
+  default     = 0
+}
 
 variable "username" {
   type        = string
